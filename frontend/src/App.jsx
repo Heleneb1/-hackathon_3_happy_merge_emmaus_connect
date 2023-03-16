073ef7db5b356afca20e09d721b43929a4de0670
sarahcharbit <sarahcharbit000@gmail.com>
2023-03-16 15:41:13 +0100

    good

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import { Routes, Route } from "react-router-dom";
-import Header from "@components/Header";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Calculatrice from "./pages/Calculatrice";
+import Tableau from "./pages/Tableau";
 
 import "./App.scss";
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/calculatrice" element={<Calculatrice />} />
+        <Route path="/tableau" element={<Tableau />} />
       </Routes>
     </div>
   );
--- frontend/src/pages/Tableau.jsx
+++ frontend/src/pages/Tableau.jsx
@@ -1,5 +1,73 @@
+import { useForm } from "react-hook-form";
+
 function Tableau() {
-  return <div />;
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <div className="Global">
+      <div className="containerTableau">
+        <form className="containerForm" onSubmit={handleSubmit(onSubmit)}>
+          <input
+            className="inputForm"
+            placeholder="ID"
+            {...register("ID", { required: true })}
+          />
+          <input
+            className="inputForm"
+            type="text"
+            placeholder="Donateur"
+            {...register("Donateur", { required: true })}
+          />
+          <input
+            className="inputForm"
+            type="text"
+            placeholder="Marque"
+            {...register("Marque")}
+          />
+          <input
+            className="inputForm"
+            type="text"
+            placeholder="Modele"
+            {...register("Modele")}
+          />
+          <input
+            className="inputForm"
+            type="text"
+            placeholder="Ecran"
+            {...register("Ecran", {})}
+          />
+          <select
+            className="inputForm"
+            placeholder="Reseau"
+            {...register("Reseau", {})}
+          >
+            <option>Selectionnez le réseau</option>
+            <option value="3G">3G</option>
+            <option value="4G">4G</option>
+            <option value="5G">5G</option>
+          </select>
+
+          <select
+            className="inputForm"
+            type="text"
+            placeholder="Catégorie"
+            {...register("Catégorie", {})}
+          >
+            <option>Selectionnez la catégorie</option>
+            <option value="1">1 - HC</option>
+            <option value="2">2 - C</option>
+            <option value="3">3 - B</option>
+            <option value="4">4 - A</option>
+            <option value="5">5 - Premium</option>
+          </select>
+        </form>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input className="buttonForm" type="submit"></input>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Tableau;
