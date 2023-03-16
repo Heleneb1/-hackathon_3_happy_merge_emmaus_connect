d7926b4c394baf5a2a478313addea5ccffae9ad7
CCeciliaDev <ceciliadev@outlook.com>
2023-03-16 17:42:47 +0100

    update filter in table phone

--- frontend/src/pages/ListPhones.jsx
+++ frontend/src/pages/ListPhones.jsx
@@ -1,20 +1,44 @@
-import Header from "@components/Header";
-import axios from "axios";
 import { useState, useEffect } from "react";
+import axios from "axios";
+import { BiDownArrow } from "react-icons/bi";
+import Header from "../components/Header";
 
 export default function ListPhones() {
   const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     axios
-      .get("http://localhost:5000/phone")
+      .get("http://localhost:5000/phones")
       .then((res) => {
         setData(res.data);
+        setFilteredData(res.data);
+
+        const uniqueCategories = Array.from(
+          new Set(res.data.map((phone) => phone.categorie))
+        );
+        setCategories(uniqueCategories);
       })
       .catch((error) => {
         console.error("Erreur lors de la récupération des données:", error);
       });
   }, []);
+
+  useEffect(() => {
+    if (selectedCategory === "") {
+      setFilteredData(data);
+    } else {
+      setFilteredData(
+        data.filter((phone) => phone.categorie === selectedCategory)
+      );
+    }
+  }, [selectedCategory, data]);
+
+  const handleCategoryChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
 
   return (
     <div className="mainDivList">
@@ -22,7 +46,26 @@ export default function ListPhones() {
       <div className="divListPhones">
         <div className="divListTable">
           <h1>Listes des téléphones</h1>
-          <input type="submit" className="listExportButton" value="Export" />
+
+          <div className="filterPhonesContainer">
+            <select
+              id="categoryFilter"
+              className="categoryFilterPhones"
+              value={selectedCategory}
+              onChange={handleCategoryChange}
+            >
+              <option value="">Filtrer par catégories</option>
+              {categories.map((category) => (
+                <option key={category} value={category}>
+                  {category}
+                </option>
+              ))}
+            </select>
+            <div className="selectArrow">
+              <BiDownArrow />
+            </div>
+          </div>
+
           <div className="tableList">
             <div className="tableList-header">
               <div className="headerList-item">Marque</div>
@@ -31,9 +74,9 @@ export default function ListPhones() {
               <div className="headerList-item">Stockage</div>
               <div className="headerList-item">Ecran</div>
               <div className="headerList-item">Etat</div>
-              <div className="headerList-item">Categorie</div>
+              <div className="headerList-item">Catégorie</div>
             </div>
-            {data.map((row) => (
+            {filteredData.map((row) => (
               <div key={row.id} className="tableList-row">
                 <div className="rowList-item">{row.marque}</div>
                 <div className="rowList-item">{row.modele}</div>
@@ -45,6 +88,7 @@ export default function ListPhones() {
               </div>
             ))}
           </div>
+          <input type="submit" className="listExportButton" value="Export" />
         </div>
       </div>
     </div>
