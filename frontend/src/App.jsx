7ee224d67543452088df25dd593f780cab791f85
CCeciliaDev <ceciliadev@outlook.com>
2023-03-16 09:45:23 +0100

    update header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,17 @@
 import { Routes, Route } from "react-router-dom";
-import Header from "@components/Header";
 import Home from "./pages/Home";
 import Calculatrice from "./pages/Calculatrice";
 
 import "./App.scss";
+import ListPhones from "@pages/ListPhones";
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/calculatrice" element={<Calculatrice />} />
+        <Route path="/listphones" element={<ListPhones />} />
       </Routes>
     </div>
   );
