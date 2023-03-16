07f0447c505a0324a5e6325d16c62ca7e8af9de1
Newaluigi <alexandre.levillain@hotmail.com>
2023-03-16 04:41:26 +0100

    home creation

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,76 @@
+import emmauslogo from "../assets/emmaus.png";
+
 export default function Home() {
-  return <header className="App-header">Hello</header>;
+  return (
+    <div className="homeBody">
+      <div className="background">
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <div className="logo">
+          <img src={emmauslogo} alt="logo emmaus" />
+        </div>
+      </div>
+
+      <div className="form">
+        <h2>Sign up</h2>
+        <input type="email" placeholder="Adresse mail" />
+        <input type="password" placeholder="Mot de passe" />
+        <div className="valider">
+          <input type="submit" className="validButton" value="Valider" />
+        </div>
+        <p className="access">Demande d'accès</p>
+      </div>
+      <div className="circle" />
+      <div className="circle-1" />
+      <div className="circle-2" />
+    </div>
+  );
 }
