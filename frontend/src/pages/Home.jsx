cbd172c684e4938c56adb7a16c91e1edcad181c1
Helene <helene.blanche@hotmail.fr>
2023-03-16 15:46:06 +0100

    first commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,21 @@
+import { useState } from "react";
+import ModalAntutu from "../components/ModalAntutu";
+
 export default function Home() {
-  return <header className="App-header">Hello</header>;
+  const [openModal, setOpenModal] = useState(false);
+  return (
+    <div className="Modal">
+      <h1>Cliquer ici</h1>
+      <input
+        className="openModalBtn"
+        type="button"
+        value="Ouvrir"
+        onClick={() => {
+          setOpenModal(true);
+        }}
+      />
+
+      {openModal && <ModalAntutu setOpenModal={setOpenModal} />}
+    </div>
+  );
 }
